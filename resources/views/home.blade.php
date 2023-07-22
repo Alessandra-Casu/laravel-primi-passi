2fe8266298954693fc471b55b0447b70d0867e99
Alessandra-Casu <alessandracasu919@gmail.com>
2023-07-22 16:15:25 +0200

    commit di ripasso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
-Hello world
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Benvenuto </h1>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/Page', function () {
+    return view('FirstPage');
 });
